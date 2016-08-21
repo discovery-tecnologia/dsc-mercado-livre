c9b2591130c1ef2145a116edb1d8b95e874734e4
dilowagner <diegowagner4@gmail.com>
2016-08-21 19:43:05 -0300

    Enviando classe credentials

---
+++ src/Credentials.php
@@ -0,0 +1,36 @@
+<?php
+namespace Dsc\MercadoLivre;
+
+/**
+ * @author Diego Wagner <diegowagner4@gmail.com>
+ */
+class Credentials
+{
+    /**
+     * @var string
+     */
+    protected $clientId;
+
+    /**
+     * @var string
+     */
+    protected $clientSecret;
+
+    /**
+     * @var string
+     */
+    protected $accessToken;
+
+    /**
+     * @var string
+     */
+    protected $refreshToken;
+
+    public function __construct($clientId, $clientSecret, $accessToken = null, $refreshToken = null)
+    {
+        $this->clientId     = $clientId;
+        $this->clientSecret = $clientSecret;
+        $this->accessToken  = $accessToken;
+        $this->refreshToken = $refreshToken;
+    }
+}
