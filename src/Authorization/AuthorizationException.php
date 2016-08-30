fe8a1fd4c924e3e1d5ee898bbbf527fc944f9585
DiloWagner <diegowagner4@gmail.com>
2016-08-29 23:51:11 -0300

    Enviando metodo getAccessToken para autorizacao

---
+++ src/Authorization/AuthorizationException.php
@@ -0,0 +1,10 @@
+<?php
+namespace Dsc\MercadoLivre\Authorization;
+
+/**
+ * @author Diego Wagner <diegowagner4@gmail.com>
+ */
+class AuthorizationException extends \Exception
+{
+
+}
--- src/Authorization/AuthorizationService.php
+++ src/Authorization/AuthorizationService.php
@@ -1,6 +1,10 @@
 <?php
 namespace Dsc\MercadoLivre\Authorization;
 
+use Doctrine\Common\Cache\Cache;
+use Doctrine\Common\Cache\FilesystemCache;
+use Dsc\MercadoLivre\Client;
+use Dsc\MercadoLivre\Credentials;
 use Dsc\MercadoLivre\Service;
 use Psr\Http\Message\StreamInterface;
 
@@ -10,9 +14,20 @@ use Psr\Http\Message\StreamInterface;
 class AuthorizationService extends Service
 {
     /**
+     * @var Cache
+     */
+    private $cache;
+
+    /**
      * @var string
      */
     private $redirectUri;
+
+    public function __construct(Credentials $credentials, Client $client, Cache $cache = null)
+    {
+        parent::__construct($credentials, $client);
+        $this->cache = $cache ?: new FilesystemCache(sys_get_temp_dir());
+    }
 
     /**
      * @param string $resource
@@ -81,5 +96,64 @@ class AuthorizationService extends Service
 
         $oAuthUri = $this->credentials->getEnvironment()->getOAuthUri();
         return $this->post($oAuthUri, $params);
+    }
+
+    /**
+     * @param null $code
+     * @return mixed
+     * @throws AuthorizationException
+     */
+    public function getAccessToken($code = null)
+    {
+        $accessToken = $this->cache->fetch('access_token');
+        $credential  = $this->credentials->getCredential();
+        //$accessToken = $credential->getAccessToken();
+        // se existir o parametro code ou um token de acesso na sessao
+        if(!$code && !$accessToken) {
+            throw new AuthorizationException('User not authenticate.');
+        }
+
+        // Se existe o parametro code e a sessao esta vazia
+        if($code && !($accessToken))  {
+
+            // faz um pedido de autorizacao a API
+            $response = $this->authorize($code, '/mercado-livre/autorize/');
+            $this->processResponseAndCached($response);
+            $credential->setAccessToken($response['body']->access_token);
+            $credential->setRefreshToken($response['body']->refresh_token);
+
+        } else {
+
+            // se o token de acesso expirou
+            if($this->cache->fetch('expires_in') < time()) {
+                try {
+                    // Make the refresh proccess
+                    $response = $this->refreshAccessToken();
+                    $this->processResponseAndCached($response);
+                    $credential->setAccessToken($response['body']->access_token);
+                    $credential->setRefreshToken($response['body']->refresh_token);
+
+                } catch (\Exception $e) {
+                    throw new AuthorizationException($e->getMessage());
+                }
+            }
+        }
+
+        return $this->cache->fetch('access_token');
+    }
+
+    /**
+     * @param $response
+     * @throws AuthorizationException
+     */
+    private function processResponseAndCached($response)
+    {
+        if(! isset($response['httpCode']) && $response['httpCode'] != 200 ) {
+            throw new AuthorizationException('Response is invalid to method authorize.');
+        }
+        // save data in cache
+        $this->cache->save('access_token', $response['body']->access_token);
+        $this->cache->save('expires_in', time() + $response['body']->expires_in);
+        $this->cache->save('refresh_token', $response['body']->refresh_token);
     }
 }
