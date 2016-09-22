ed1aaad2d41acef6a9be29c4f6a745cce05b172a
DiloWagner <diegowagner4@gmail.com>
2016-09-21 22:32:01 -0300

    Enviando Product model

---
+++ src/Product/Product.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Class Product
+ *
+ * @author Diego Wagner <diegowagner4@gmail.com>
+ * http://www.discoverytecnologia.com.br
+ */
+namespace Dsc\MercadoLivre\Product;
+
+class Product
+{
+
+}
