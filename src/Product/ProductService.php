bf98ca3dcf89bab1fc8764d64d86213b73629ca5
DiloWagner <diegowagner4@gmail.com>
2016-09-21 22:30:54 -0300

    Enviando ProductService

---
+++ src/Product/ProductService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Class ProductService
+ *
+ * @author Diego Wagner <diegowagner4@gmail.com>
+ * http://www.discoverytecnologia.com.br
+ */
+namespace Dsc\MercadoLivre\Product;
+
+use Dsc\MercadoLivre\Service;
+
+class ProductService extends Service
+{
+
+}
