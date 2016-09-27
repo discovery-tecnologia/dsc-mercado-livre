f7dfc0bf2c1b874bb404265464ac5328614dc3a9
DiloWagner <diegowagner4@gmail.com>
2016-09-26 22:33:42 -0300

    Atualizando mapeamento de Produtos

---
+++ src/Product/Geolocation.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Class Geolocation
+ *
+ * @author Diego Wagner <diegowagner4@gmail.com>
+ * http://www.discoverytecnologia.com.br
+ */
+namespace Dsc\MercadoLivre\Product;
+
+use JMS\Serializer\Annotation as JMS;
+
+class Geolocation
+{
+    /**
+     * @var double
+     * @JMS\Type("double")
+     */
+    private $latitude;
+
+    /**
+     * @var double
+     * @JMS\Type("double")
+     */
+    private $longitude;
+
+    /**
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param float $latitude
+     * @return Geolocation
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param float $longitude
+     * @return Geolocation
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+        return $this;
+    }
+}
