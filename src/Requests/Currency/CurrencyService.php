f52a505be42ae862d0390e8cc9142174e0d69c17
DiloWagner <diegowagner4@gmail.com>
2016-10-05 20:42:48 -0300

    Enviando CurrencyService

---
+++ src/Requests/Currency/CurrencyService.php
@@ -0,0 +1,32 @@
+<?php
+namespace Dsc\MercadoLivre\Requests\Currency;
+
+use Doctrine\Common\Collections\Collection;
+use Dsc\MercadoLivre\Requests\RequestService;
+use Dsc\MercadoLivre\Requests\Service;
+
+class CurrencyService extends Service implements RequestService
+{
+    /**
+     * @return Collection<Currency>
+     */
+    public function findCurrencies()
+    {
+        $builder = new CurrencyResponseBuilder(
+            $this->get('/currencies')
+        );
+        return $builder->getResponse();
+    }
+
+    /**
+     * @param string $code
+     * @return Currency
+     */
+    public function findCurrency($code)
+    {
+        $builder = new CurrencyResponseBuilder(
+            $this->get(sprintf('/currencies/%s', $code))
+        );
+        return $builder->getResponse();
+    }
+}
