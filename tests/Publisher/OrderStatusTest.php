25e7d87ce537a47e73b335ac8df3dc0138713859
AnthonyRodrigues <matheus.tere7@gmail.com>
2018-09-03 12:14:37 -0300

    Feature: Creating the status order.

---
+++ tests/Publisher/OrderStatusTest.php
@@ -0,0 +1,25 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: anthonyrodrigues
+     * Date: 9/3/18
+     * Time: 12:08 PM
+     */
+
+    namespace Dsc\MercadoLivre\Publisher;
+
+    use Dsc\MercadoLivre\Announcement\OrderStatus;
+
+    class OrderStatusTest extends \PHPUnit_Framework_TestCase
+    {
+
+        public function testIsValidReturnFalseWhenStatusNotExists()
+        {
+            $this->assertFalse(OrderStatus::isValid('status_not_exists'));
+        }
+
+        public function testIsValidReturnTrueWhenStatusExists()
+        {
+            $this->assertTrue(OrderStatus::PAID);
+        }
+    }
