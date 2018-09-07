605d265d72154cd70787fe765e098d7cfc355bbd
Diego Wagner <diegowagner4@gmail.com>
2018-09-07 10:59:20 -0300

    Ajuste na formatacao do codigo

--- tests/Resources/Order/OrderStatusTest.php
+++ tests/Resources/Order/OrderStatusTest.php
@@ -1,25 +1,23 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: anthonyrodrigues
-     * Date: 9/3/18
-     * Time: 12:08 PM
-     */
+/**
+ * Class OrderStatusTest
+ *
+ * @author anthonyrodrigues
+ */
+namespace Dsc\MercadoLivre\Resources\Order;
 
-    namespace Dsc\MercadoLivre\Resources\Order;
+use Dsc\MercadoLivre\Resources\Order\OrderStatus;
 
-    use Dsc\MercadoLivre\Resources\Order\OrderStatus;
+class OrderStatusTest extends \PHPUnit_Framework_TestCase
+{
 
-    class OrderStatusTest extends \PHPUnit_Framework_TestCase
+    public function testIsValidReturnFalseWhenStatusNotExists()
     {
-
-        public function testIsValidReturnFalseWhenStatusNotExists()
-        {
-            $this->assertFalse(OrderStatus::isValid('status_not_exists'));
-        }
-
-        public function testIsValidReturnTrueWhenStatusExists()
-        {
-            $this->assertTrue(OrderStatus::isValid(OrderStatus::PAID));
-        }
+        $this->assertFalse(OrderStatus::isValid('status_not_exists'));
     }
+
+    public function testIsValidReturnTrueWhenStatusExists()
+    {
+        $this->assertTrue(OrderStatus::isValid(OrderStatus::PAID));
+    }
+}
