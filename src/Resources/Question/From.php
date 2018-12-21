0c2373c2f8e25c4b888352f14f5d0ed365593a90
Diego Wagner <diegowagner4@gmail.com>
2018-12-21 12:31:07 -0200

    Criando o objeto From

---
+++ src/Resources/Question/From.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Class Order
+ *
+ * @author Diego Wagner <diegowagner4@gmail.com>
+ * http://www.discoverytecnologia.com.br
+ */
+namespace Dsc\MercadoLivre\Resources\Question;
+
+use JMS\Serializer\Annotation as JMS;
+
+class From        
+{
+    /**
+     * @var int
+     * @JMS\Type("integer")
+     */
+    private $id;
+
+    /**
+     * @var int
+     * @JMS\Type("integer")
+     */
+    private $answeredQuestions;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return From
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAnsweredQuestions()
+    {
+        return $this->answeredQuestions;
+    }
+
+    /**
+     * @param int $answeredQuestions
+     * @return From
+     */
+    public function setAnsweredQuestions($answeredQuestions)
+    {
+        $this->answeredQuestions = $answeredQuestions;
+        return $this;
+    }
+}
