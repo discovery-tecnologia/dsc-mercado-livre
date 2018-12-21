350169d1132f8accb62caadc50ed1d9051df2d4b
Diego Wagner <diegowagner4@gmail.com>
2018-12-21 12:05:56 -0200

    Criando o objeto Answer

---
+++ src/Resources/Question/Answer.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Class Order
+ *
+ * @author Diego Wagner <diegowagner4@gmail.com>
+ * http://www.discoverytecnologia.com.br
+ */
+namespace Dsc\MercadoLivre\Resources\Question;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use JMS\Serializer\Annotation as JMS;
+
+class Answer        
+{
+    /**
+     * @var \DateTime
+     * @JMS\Type("DateTime<'Y-m-d\TH:i:s.000-04:00'>")
+     */
+    private $dateCreated;
+
+    /**
+     * @var string
+     * ACTIVE e DISABLED
+     * @JMS\Type("string")
+     */
+    private $status;
+
+    /**
+     * @var string
+     * @JMS\Type("string")
+     */
+    private $text;    
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * @param \DateTime $dateCreated
+     * @return Answer
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     * @return Answer
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     * @return Answer
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+        return $this;
+    }
+}
