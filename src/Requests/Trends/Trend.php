9c9ed64c18ce0260ecc6f65ba01798aa517a2d6d
Rafael Acioly <aciolyr@gmail.com>
2021-04-18 19:31:59 -0300

    adds trend class

---
+++ src/Requests/Trends/Trend.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Dsc\MercadoLivre\Requests\Trends;
+
+
+class Trend
+{
+    /**
+     * @var string
+     * @JMS\Type("string")
+     */
+    private $keyword;
+
+    /**
+     * @var string
+     * @JMS\Type("string")
+     */
+    private $url;
+
+    /**
+     * @param string $keyword
+     */
+    public function setKeyword($keyword)
+    {
+        $this->keyword = $keyword;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKeyword()
+    {
+        return $this->keyword;
+    }
+
+    /**
+     * @param string $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
